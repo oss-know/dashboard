20ebb19d7b024692bbccc5e816ad56c9da9efe5d
Zhen Ju <juzhen@huawei.com>
2022-03-31 17:56:46 +0800

    Add dynamic table(rendered according to the sql result)

--- src/pages/LowCodePlatform/DynamicDataTable.tsx
+++ src/pages/LowCodePlatform/DynamicDataTable.tsx
@@ -6,20 +6,42 @@ type RowBase = {
 
 type ActivityRow = RowBase & CKData.Activity;
 
+export function parseTableData(result: { columns: [][]; data: [][] }) {
+  const tableColumns: object[] = result.columns.map((colInfo: string[]) => ({
+    title: colInfo[0],
+    dataIndex: colInfo[0],
+  }));
+
+  const tableData: object[] = result.data.map((dataItem, dataIndex) => {
+    const rowData = { key: dataIndex };
+    result.columns.forEach((colInfo: string[], colIndex) => {
+      const colName = colInfo[0]; // colInfo[1] is col type, might be used in the future
+      const colType = colInfo[1];
+      // If dataVal's type is declared to 'number|string', toFixed method won't apply
+      let dataVal: any = dataItem[colIndex];
+      if (colType === 'Float64') {
+        dataVal = parseFloat(dataVal.toFixed(2));
+      }
+      rowData[colName] = dataVal;
+    });
+    return rowData;
+  });
+
+  return { tableColumns, tableData };
+}
+
 export default class DynamicDataTable extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
 
     this.state = {
       tableData: [],
-      columns: [
-        { title: 'foo', dataIndex: 'foo' },
-        { title: 'bar', dataIndex: 'bar' },
-      ],
+      columns: [],
     };
 
     this.handleRowClick = this.handleRowClick.bind(this);
   }
+
   handleRowClick(row: any) {
     console.log('row clicked:', row);
   }
@@ -27,9 +49,11 @@ export default class DynamicDataTable extends React.Component<any, any> {
   render() {
     return (
       <ProTable
-        columns={this.state.columns}
-        dataSource={this.state.tableData}
+        columns={this.props.columns}
+        dataSource={this.props.tableData}
         search={false}
+        scroll={{ x: 1000 }}
+        pagination={{ pageSize: 8 }}
         onRow={(row: ActivityRow) => {
           return {
             onClick: () => {
--- src/pages/LowCodePlatform/index.tsx
+++ src/pages/LowCodePlatform/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import type { ProColumns } from '@ant-design/pro-table';
-import ProTable from '@ant-design/pro-table';
-import { getActivities, getAllOwners, getRepos } from '@/services/clickhouse';
+import { runSql } from '@/services/clickhouse';
 import { Col, Collapse, Row } from 'antd';
 
-import DynamicDataTable from '@/pages/LowCodePlatform/DynamicDataTable';
+import DynamicDataTable, { parseTableData } from '@/pages/LowCodePlatform/DynamicDataTable';
 import SQLEditor from '@/pages/LowCodePlatform/SQLEditor';
 
 const { Panel } = Collapse;
@@ -14,17 +12,17 @@ export default class Index extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     this.state = {
-      owner: '',
-      repo: '',
-      githubID: '',
-      githubLogin: '',
-      email: '',
-      repos: [],
+      tableColumns: [],
       tableData: [],
     };
 
-    getAllOwners().then((owners) => {
-      this.setState({ allOwners: owners.map((owner) => ({ value: owner, label: owner })) });
+    this.sqlUpdate = this.sqlUpdate.bind(this);
+  }
+
+  sqlUpdate(newSql: string) {
+    runSql(newSql).then((result) => {
+      const tableResult = parseTableData(result);
+      this.setState({ ...tableResult });
     });
   }
 
@@ -35,14 +33,14 @@ export default class Index extends React.Component<any, any> {
           <Col span={24}>
             <Collapse>
               <Panel header="SQL Editor" key="1">
-                <SQLEditor />
+                <SQLEditor onSqlUpdate={this.sqlUpdate} />
               </Panel>
             </Collapse>
           </Col>
         </Row>
         <Row>
           <Col span={24}>
-            <DynamicDataTable />
+            <DynamicDataTable columns={this.state.tableColumns} tableData={this.state.tableData} />
           </Col>
         </Row>
 
