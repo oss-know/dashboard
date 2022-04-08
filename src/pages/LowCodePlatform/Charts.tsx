c6b819abfeb042d07549ac915d3b2753eb01f051
Zhen Ju <juzhen@huawei.com>
2022-04-08 16:33:12 +0800

    Add util function to parse activities. Since activity data is passed to radar chart, the data has to be regulated(calculate the min, max value, and map all values to [min,max])

--- src/pages/LowCodePlatform/Charts.tsx
+++ src/pages/LowCodePlatform/Charts.tsx
@@ -1,6 +1,50 @@
 import { Radar } from '@ant-design/plots';
 import React from 'react';
 
+export function parseActivities(rawActivities: CKData.Activity[]) {
+  let maxs: object = {};
+  let mins: object = {};
+
+  rawActivities.forEach((act) => {
+    [
+      'code_contribution',
+      'code_tweaking',
+      'issue_coordination',
+      'issue_reporting',
+      'knowledge_sharing',
+      'progress_control',
+    ].forEach((key) => {
+      if (!maxs[key] || maxs[key] < act[key]) {
+        maxs[key] = act[key];
+      }
+      if (!mins[key] || mins[key] > act[key]) {
+        mins[key] = act[key];
+      }
+    });
+  });
+  const parsedActivities: object[] = rawActivities.map((act) => {
+    [
+      'code_contribution',
+      'code_tweaking',
+      'issue_coordination',
+      'issue_reporting',
+      'knowledge_sharing',
+      'progress_control',
+    ].forEach((key) => {
+      const max = maxs[key];
+      const min = mins[key];
+      if (max == min) {
+        act[key] = max;
+      } else {
+        act[key] = ((act[key] - min) / (max - min)) * 100;
+      }
+      act[key] = parseFloat(act[key].toFixed(2));
+    });
+    return act;
+  });
+  return parsedActivities;
+}
+
 function activityToRadarData(activity: CKData.Activity): object[] {
   return [
     'codeContribution',
