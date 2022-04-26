ff8fbe87fdd968484df17641aeb87f94c9384adf
Zhen Ju <juzhen@huawei.com>
2022-04-26 15:17:49 +0800

    Mapping the activities data(mapping to range[0, 100]) in LowCodePlatform

--- src/pages/LowCodePlatform/Charts.tsx
+++ src/pages/LowCodePlatform/Charts.tsx
@@ -22,6 +22,7 @@ export function parseActivities(rawActivities: CKData.Activity[]) {
       }
     });
   });
+
   const parsedActivities: object[] = rawActivities.map((act) => {
     [
       'code_contribution',
