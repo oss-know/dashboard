0dfc9f348513596c01afc0b69da7fcf9c8c06653
Zhen Ju <juzhen@huawei.com>
2022-05-20 18:30:59 +0800

    Add execute button for SQL editor. Enhance the intro of ctrl+enter

--- src/locales/en-US/lowcodePlatform.ts
+++ src/locales/en-US/lowcodePlatform.ts
@@ -1,4 +1,5 @@
 export default {
   'lowcodePlatform.sqlEditor': 'SQL Editor',
+  'lowcodePlatform.executeSQL': 'Execute SQL',
   'lowcodePlatform.sqlEditorIntro': 'Press Ctrl + Enter to execute the SQL',
 };
