e2df8e9b96b9c6bcb05b0fba229343634bdcf5cc
Zhen Ju <juzhen@huawei.com>
2022-05-23 14:51:51 +0800

    Add tz dist for tags in second dir table.
    This is a temp solution, with quick & dirty approaches:
    - The cell data for each column is packed with redundent propery
      'owner', 'repo', and 'type'
    - The tooltip 'title' property is assigned to a component with it's
      inner network requests and life cycle, which might be too heavy

--- src/pages/ContribDistribution/SecondaryDirsTable.tsx
+++ src/pages/ContribDistribution/SecondaryDirsTable.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Divider, Table, Tag } from 'antd';
+import { Divider, Table, Tag, Tooltip } from 'antd';
 import styles from '@/pages/ContribDistribution/index.less';
 import { getIntl } from 'umi';
+import {
+  ownerRepoDirDomainDeveloperTzDistSql,
+  ownerRepoDirDomainFileTzDistSql,
+  ownerRepoDirRegionDeveloperTzDistSql,
+  ownerRepoDirRegionFileTzDistSql,
+} from '@/pages/ContribDistribution/DataSQLs';
+import { runSql } from '@/services/clickhouse';
 
 const intl = getIntl();
 const SECONDARY_DIR_TABLE_COLS = [
@@ -31,23 +38,122 @@ const SECONDARY_DIR_TABLE_COLS = [
   },
 ];
 
+const REGION_MAP = {};
+REGION_MAP['欧洲西部'] = 'WEST_EUROPE';
+REGION_MAP['欧洲东部'] = 'EAST_EUROPE';
+REGION_MAP['澳洲'] = 'AUSTRALIA';
+REGION_MAP['北美'] = 'NORTH_AMERICA';
+REGION_MAP['日韩'] = 'JP_KR';
+REGION_MAP['印度'] = 'INDIA';
+REGION_MAP['中国'] = 'CHINA';
+
+function getTagTzDist(owner, repo, secondaryDir, type, key, callback) {
+  let title = '';
+  switch (type) {
+    case 'file_region_dist':
+      runSql(ownerRepoDirRegionFileTzDistSql(owner, repo, secondaryDir, REGION_MAP[key])).then(
+        (result) => {
+          title = result.data
+            .map((item) => {
+              const tz = item[0];
+              const count = item[1];
+              const tzStr = tz > 0 ? `+${tz}` : `${tz}`;
+              return `${tzStr}: ${count}`;
+            })
+            .join(', ');
+          callback(title);
+        },
+      );
+      break;
+    case 'file_domain_dist':
+      runSql(ownerRepoDirDomainFileTzDistSql(owner, repo, secondaryDir, key)).then((result) => {
+        title = result.data
+          .map((item) => {
+            const tz = item[0];
+            const count = item[1];
+            const tzStr = tz > 0 ? `+${tz}` : `${tz}`;
+            return `${tzStr}: ${count}`;
+          })
+          .join(', ');
+        callback(title);
+      });
+      break;
+    case 'developer_region_dist':
+      runSql(ownerRepoDirRegionDeveloperTzDistSql(owner, repo, secondaryDir, REGION_MAP[key])).then(
+        (result) => {
+          title = result.data
+            .map((item) => {
+              const tz = item[0];
+              const count = item[1];
+              const tzStr = tz > 0 ? `+${tz}` : `${tz}`;
+              return `${tzStr}: ${count}`;
+            })
+            .join(', ');
+          callback(title);
+        },
+      );
+      break;
+    case 'developer_domain_dist':
+      runSql(ownerRepoDirDomainDeveloperTzDistSql(owner, repo, secondaryDir, key)).then(
+        (result) => {
+          title = result.data
+            .map((item) => {
+              const tz = item[0];
+              const count = item[1];
+              const tzStr = tz > 0 ? `+${tz}` : `${tz}`;
+              return `${tzStr}: ${count}`;
+            })
+            .join(', ');
+          callback(title);
+        },
+      );
+      break;
+    default:
+      break;
+  }
+}
+
+class TzDistTitle extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+    };
+    const { owner, repo, dir, type, regionKey } = this.props;
+    getTagTzDist(owner, repo, dir, type, regionKey, (title) => {
+      this.setState({ title });
+    });
+  }
+  render() {
+    return <div>{this.state.title}</div>;
+  }
+}
+
 function secondaryDirTableCellRender(cellData, rowData, index) {
-  return cellData.map((regionInfo, index) => {
-    const { region, value } = regionInfo;
-    const line = `${region}: ${value}`;
-    const key = `${rowData.secondaryDir}-${region}-${line}`;
+  const { secondaryDir } = rowData;
+  return cellData.map((info, index) => {
+    const { key, value, type, owner, repo } = info;
+    const line = `${key}: ${value}`;
+    const childKey = `${rowData.secondaryDir}-${key}-${index}`;
     // TODO It's super weried that JS always complain 'each children in list should have uniq key'
     // TODO while it's really 'uniq' enough
     let color = 'volcano';
-    if (typeof region == 'string' && region.toLowerCase().indexOf('huawei') != -1) {
+    if (typeof key == 'string' && key.toLowerCase().indexOf('huawei') != -1) {
       color = '#108ee9';
     }
     return (
-      // <div key={key}>
-      <Tag key={key} color={color}>
-        {line}
-      </Tag>
-      // </div>
+      <Tooltip
+        key={childKey}
+        trigger="click"
+        title={
+          <TzDistTitle owner={owner} repo={repo} dir={secondaryDir} type={type} regionKey={key} />
+        }
+      >
+        <Tag key={key} color={color}>
+          {line}
+        </Tag>
+      </Tooltip>
     );
   });
 }
